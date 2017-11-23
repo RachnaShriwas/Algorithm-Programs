7e8e576b92e51d9daeb3a59d307a9002988f3f0e
Rachna Shriwas <rachnashriwas@gmail.com>
2017-11-23 14:55:23 +0530

    added DFS and Dijkstra

---
+++ Graphs/dfs.cpp
@@ -0,0 +1,107 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+static int t = 0;
+
+void dfs_visit(vector<int> adj[], char color[], int pie[], int u, int d[], int f[]) {
+
+        color[u] = 'y';
+        d[u] = ++t;
+        cout<<u<<" ";
+        vector<int>::iterator it;
+        for(it = adj[u].begin(); it != adj[u].end(); ++it) {
+           // cout<<"\n"<<"i = "<<*it;
+            if(color[*it] == 'r') {
+                pie[*it] = u;
+                dfs_visit(adj, color, pie, *it, d, f);
+            }
+        }
+
+        color[u] = 'b';
+        f[u] = ++t;
+}
+
+void graph_traversal(int size ,vector<int> adj[], int v[]) {
+    //cout<<"size: "<<length<<endl;
+    char color[size];
+    int pie[size];
+    int d[size];
+    int f[size];
+    memset(d, 0, sizeof(d));
+    memset(f, 0, sizeof(f));
+
+    for(int i = 0; i < size; i++) {
+        color[v[i]] = 'r';
+        //cout<<"color set: "<<v[i]<<endl;
+        pie[v[i]] = v[i];
+    }
+
+    int s = v[0];
+    pie[s] = s;
+
+    dfs_visit(adj, color, pie, s, d, f);
+
+    for(int i = 0; i < size; i++) {
+        if(color[v[i]] == 'r') {
+            if(adj[v[i]].size() == 0) {
+                pie[v[i]] = v[i];
+                continue;
+            }
+            dfs_visit(adj, color, pie, v[i], d, f);
+        }
+    }
+
+    //loop for single nodes
+    for(int i = 0; i < size; i++) {
+        if(color[v[i]] == 'r') {
+            color[v[i]] = 'b';
+            d[v[i]] = ++t;
+            f[v[i]] = ++t;
+        }
+    }
+
+    cout<<"\nParent vertices: "<<endl;
+    for(int i = 0; i < size; i++) {
+        cout<<"pie of "<<v[i]<<" is: "<<pie[v[i]]<<endl;
+    }
+
+    cout<<"\nVisit Time: "<<endl;
+    for(int i = 0; i < size; i++) {
+        cout<<"Visit time of "<<v[i]<<" is: "<<d[v[i]]<<endl;
+    }
+
+    cout<<"\nFinish Time: "<<endl;
+    for(int i = 0; i < size; i++) {
+        cout<<"Finish Time of "<<v[i]<<" is: "<<f[v[i]]<<endl;
+    }
+}
+
+
+int main() {
+    /* Graph edge list
+        1-->2
+        1-->4
+        2-->3
+        2-->4
+        4-->5
+        5-->2
+        5-->3
+        7-->6
+    */
+
+    int V = 7;
+    int vert[] = {1,2,3,4,5,6,7};
+    vector<int> adj[V+1];
+
+    adj[1].push_back(2);
+    adj[1].push_back(4);
+    adj[2].push_back(3);
+    adj[2].push_back(4);
+    adj[4].push_back(5);
+    adj[5].push_back(2);
+    adj[5].push_back(3);
+    adj[7].push_back(6);
+
+    graph_traversal(V, adj, vert);
+
+}
---
+++ Graphs/dijkstra.cpp
@@ -0,0 +1,87 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void dijkstra(int s, vector<pair<int, int> > adj[], int size, int v[]) {
+    int p[size+1];
+    int color[size+1];
+    int pie[size+1];
+    memset(p, INT_MAX, sizeof(p));
+    memset(color, 'r', sizeof(color));
+    memset(pie, 0, sizeof(pie));
+
+    for(int i = 1; i <= size; i++)
+        color[i] = 'r';
+
+    priority_queue< pair<int,int> , vector<pair<int,int> >, greater<pair<int,int> > > q;
+    p[s] = 0;
+    q.push(make_pair(s, p[s]));
+
+    while(!q.empty()) {
+        int u = q.top().first;
+        //cout<<"popping: "<<u<<" "<<p[u]<<endl;
+        q.pop();
+
+        vector<pair<int, int> >::iterator it;
+        for(it = adj[u].begin(); it != adj[u].end(); it++) {
+            int v = it->first;
+            //cout<<"seeing v: "<<v<<endl;
+            if(color[v] == 'r') {
+                p[v] = p[u] + it->second;
+                //cout<<"v was red, p now is: "<< p[v]<<endl;
+                pie[v] = u;
+                q.push(make_pair(v, p[v]));
+                color[v] = 'y';
+            }
+            else if(color[v] == 'y') {
+                if(p[v] > (p[u] + it->second) ) {
+                    p[v] = p[u] + it->second;
+                    pie[v] = u;
+                    //cout<<"v was yellow, p now is: "<< p[v]<<endl;
+                }
+            }
+        }
+    }
+
+    //print parent
+    cout<<"Parent vertices: "<<endl;
+    for(int i = 0; i < size; i++) {
+        cout<<"pie of "<<v[i]<<" is: "<<pie[v[i]]<<endl;
+    }
+
+    //shortest path
+    cout<<"\nShortest paths: "<<endl;
+    for(int i = 0; i < size; i++) {
+        cout<<"pie of "<<v[i]<<" is: "<<p[v[i]]<<endl;
+    }
+
+}
+
+int main() {
+    /* Graph edge list
+        edge   weight
+        1-->2   3
+        1-->4   6
+        2-->3   9
+        2-->4   2
+        4-->5   4
+        5-->2   2
+        5-->3   1
+    */
+
+    int V = 5;
+    int v[] = {1,2,3,4,5};
+
+    vector<pair<int, int> > adj[V+1];
+
+    adj[1].push_back(make_pair(2,3));
+    adj[1].push_back(make_pair(4,6));
+    adj[2].push_back(make_pair(3,9));
+    adj[2].push_back(make_pair(4,2));
+    adj[4].push_back(make_pair(5,4));
+    adj[5].push_back(make_pair(2,2));
+    adj[5].push_back(make_pair(3,1));
+
+    dijkstra(v[0], adj, V, v);
+
+    return 0;
+}
